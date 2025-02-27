ddf5b67e658686cd378fee7193116855b7833563
Alexandre DUMOUT <alexdum03@hotmail.fr>
2025-02-27 16:03:36 +0100

    [UPD] Add langhuage et city param

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -2,13 +2,24 @@ const mydatabase = require("../../database");
 
 function getUsers(req, res) {
     
+    let sql = "select * from users where 1=1";
+    const sqlValues = [];
+  
+    if (req.query.language != null) {
+      sql += " and language = ?";
+      sqlValues.push(req.query.language);
+    }
+    if (req.query.city != null) {
+        sql += " and city = ?";
+        sqlValues.push(req.query.city);
+    }
     mydatabase
-        .query("select * from users")
+        .query(sql, sqlValues)
         .then(([movies]) => {
             res.json(movies); // use res.json instead of console.log
         })
         .catch((err) => {
-            //console.error(err);
+            console.error(err);
             res.sendStatus(500);
         });
 }
