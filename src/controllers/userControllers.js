832897c15125d966f5b4881b5fd616651b5c79ef
Alexandre DUMOUT <alexdum03@hotmail.fr>
2025-02-27 11:27:39 +0100

    [FIX] Pb headers

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -4,7 +4,7 @@ function getUsers(req, res) {
     mydatabase
         .query("select * from movies")
         .then(([movies]) => {
-            res.sendStatus(200).json(movies); // use res.json instead of console.log
+            res.json(movies); // use res.json instead of console.log
         })
         .catch((err) => {
             //console.error(err);
@@ -19,7 +19,7 @@ const getUsersById = (req, res) => {
         .query("select * from movies where id = ?", [id])
         .then(([movies]) => {
         if (movies[0] != null) {
-            res.sendStatus(200).json(movies[0]);
+            res.json(movies[0]);
         } else {
             res.sendStatus(404);
         }
